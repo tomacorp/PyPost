765c00c069fb02fcd74e7c7d693068a191396767
Tom Anderson <tomacorp@gmail.com>
2015-06-07 15:57:41 -0700

    Added ImgMplCanvas test routines

--- ImgMplCanvas.py
+++ ImgMplCanvas.py
@@ -27,121 +27,149 @@ import ntpath
 class ImgMplCanvas(FigureCanvas):
   def __init__(self, parent=None, width=5, height=4, dpi=100):
     pass
-  
-class Post(QMainWindow):
-  def __init__(self, parent=None):
-    super(Post, self).__init__(parent)
 
-    self.graphs= MplCanvasDict.MplCanvasDict()
-
-    self.table = MainWin(self)
-    self.setCentralWidget(self.table)
-
-    self.createActions()
-    self.createMenus()
-
-    self.setWindowTitle('PyPost')
-
-    self.resize(1200, 420)
-    self.statusBar().showMessage('Ready')  
-  
 if __name__ == "__main__":
-  
+
   import os.path
   from matplotlib import pyplot as plt
-  
-  print("ImgMplCanvas class test")
-  img= ImgMplCanvas(parent=None, width=5, height=4, dpi=100)  
-  
-  fn= 't/yellow.png'
-  if not os.path.isfile(fn):
-    print("File " + str(fn) + " not found. Bailing out.")
-    
-  useTk= False
-  useFrame= True
 
-  fileType= imghdr.what(fn)
-  fileExtension= '.' + fileType
-  if (fn.endswith(fileExtension)):
-    imageName= ntpath.basename(fn)
-    imageName, extension= os.path.splitext(imageName)
-  else:
-    imageName= 'image1'
-  img= skimage.data.imread(fn)
-  
-  if useTk:
+  def getImageName(fn):
+
+    """I like a lot of error checking on files.
+       getImageName gets the string 'yellow' 'for t/yellow.png'.
+       It also checks to see that the file extension matches the file type
+       that Python knows how to infer."""
+
+    errmessage= ''
+    if not os.path.isfile(fn):
+      errmessage="File " + str(fn) + " not found. Bailing out."
+      return None
+    fileType= imghdr.what(fn)
+    fileExtension= '.' + fileType
+    if (fn.endswith(fileExtension)):
+      imageName= ntpath.basename(fn)
+      imageName, extension= os.path.splitext(imageName)
+    else:
+      errmessage= "File extension " + str(fileExtension) + " does not match file type " + str(fileType)
+      return None
+    return imageName
+
+  def displayImageTk(fn, imageName='image'):
+    """
+    This is the easy pyplot way to display an image.
+    It also has some nice features, like panning and zooming.
+    It is not the PySide way of doing things,
+    but it makes a nice reference for debugging.
+    """
     print("Display image: " + str(fn) + " using Tk")
+    img= skimage.data.imread(fn)
     plt.imshow(img)
     plt.show()
-    
-  elif useFrame:
-    print("Display image: " + str(fn) + " with frame and label using PySide")
-  
-    app = QApplication(sys.argv)
-      
-    height, width, depth= shape(img)   
-    fig = Figure(figsize=(width, height), dpi=100)
-    
-    axes = fig.add_subplot(111)
-    axes.set_xlim(0, width)
-    axes.set_ylim(0, height)
-    axes.imshow(img)
-    
-    main = QtGui.QMainWindow()
-    main.setGeometry(0, 100, 200+int(width*1.4), 200+int(height*1.4))
-    main.setWindowTitle(imageName)  
-    
-    frame= QtGui.QFrame()
-    frame.setFrameShape(QtGui.QFrame.StyledPanel)
-    frame.setParent(main)
-    
-    canvas = FigureCanvas(fig)
-    canvas.setParent(frame)
-    canvas.setMinimumSize(100+int(width*1.4), 100+int(height*1.4))
-    
-    label= QtGui.QLabel()
-    label.setText(imageName)   # If it is a string, it must be in unicode eg u"myname"
-    label.setAlignment(Qt.AlignHCenter)
-    label.setParent(frame)
-    
-    layout = QtGui.QVBoxLayout()
-    layout.addWidget(canvas)
-    layout.addWidget(label)
-    
-    layout.setStretchFactor(canvas, 1.0)
 
-    frame.setLayout(layout)
-    frame.show()
-    
-    main.setCentralWidget(frame)
-    main.show()
-    
-    sys.exit(app.exec_())
-    
-  else:
+  def displayImagePyside(fn, imageName='image'):
+    """
+    This is the simplest way to use PySide to display an image in a Figure subplot.
+    There is no layout code, other than hand-coding based on the size of the image.
+    """
     print("Display image: " + str(fn) + " using PySide")
-  
+    img= skimage.data.imread(fn)
+
     app = QApplication(sys.argv)
-      
-    height, width, depth= shape(img)   
+
+    height, width, depth= shape(img)
     fig = Figure(figsize=(width, height), dpi=100)
-    
+
     axes = fig.add_subplot(111)
-    axes.set_xlim(0, width)
-    axes.set_ylim(0, height)
+    # The next two lines are equivalent to the default axes settings
+    #   axes.set_xlim(0, width)
+    #   axes.set_ylim(0, height)
     axes.imshow(img)
-    
+
     main = QtGui.QMainWindow()
     # Without setGeometry the window will have a default position and size
     main.setGeometry(0, 100, 200+int(width*1.4), 200+int(height*1.4))
-    main.setWindowTitle(imageName)  
-    
+    main.setWindowTitle(imageName)
+
     canvas = FigureCanvas(fig)
     canvas.setParent(main)
     # setMinimumSize prevents the window from becoming tiny and crushing the axes
     canvas.setMinimumSize(100+int(width*1.4), 100+int(height*1.4))
-    
+
     main.setCentralWidget(canvas)
     main.show()
-    
-    sys.exit(app.exec_())    
+
+    sys.exit(app.exec_())
+
+  def displayImagePysideFrame(fn, imageName='image'):
+    """
+    This adds a QFrame and QLabel to the PySide interface, and a simple layout.
+    """
+    print("Display image: " + str(fn) + " with frame and label using PySide")
+    img= skimage.data.imread(fn)
+
+    app = QApplication(sys.argv)
+
+    height, width, depth= shape(img)
+    fig = Figure(figsize=(width, height), dpi=100)
+
+    axes = fig.add_subplot(111)
+    axes.set_xlim(0, width)
+    axes.set_ylim(0, height)
+    axes.imshow(img)
+
+    main = QtGui.QMainWindow()
+    main.setGeometry(0, 100, 200+int(width*1.4), 200+int(height*1.4))
+    main.setWindowTitle(imageName)
+
+    frame= QtGui.QFrame()
+    frame.setFrameShape(QtGui.QFrame.StyledPanel)
+    frame.setParent(main)
+
+    canvas = FigureCanvas(fig)
+    canvas.setParent(frame)
+    canvas.setMinimumSize(100+int(width*1.4), 100+int(height*1.4))
+
+    label= QtGui.QLabel()
+    # when calling QtGui.QLabel.setText() with a string, must use unicode eg u"myname"
+    label.setText(imageName)
+    label.setAlignment(Qt.AlignHCenter)
+    label.setParent(frame)
+
+    layout = QtGui.QVBoxLayout()
+    layout.addWidget(canvas)
+    layout.addWidget(label)
+
+    layout.setStretchFactor(canvas, 1.0)
+
+    frame.setLayout(layout)
+    frame.show()
+
+    main.setCentralWidget(frame)
+    main.show()
+
+    sys.exit(app.exec_())
+
+
+  # The test starts here
+  print("ImgMplCanvas class test")
+
+  # Non-OO code, use a global error message:
+  errmessage= ''
+
+  # The file is in a subdirectory of the run directory.
+  fn= 't/yellow.png'
+
+  name = getImageName(fn)
+  if name == None:
+    print(errmessage)
+    sys.exit()
+
+  useTk= False
+  useFrame= False
+
+  if useTk:
+    displayImageTk(fn, imageName=name)
+  elif useFrame:
+    displayImagePysideFrame(fn, imageName=name)
+  else:
+    displayImagePyside(fn, imageName=name)
